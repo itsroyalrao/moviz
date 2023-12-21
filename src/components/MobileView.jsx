ea83bc88e087fa23f38d7c43755eabf7ea130475
Mohit Yadav <my701319@gmail.com>
2023-12-21 19:40:34 +0530

    4th commit

--- src/components/MobileView.jsx
+++ src/components/MobileView.jsx
@@ -6,7 +6,7 @@ function MobileView() {
   return (
     <div>
       <div className="bg-yellow-300 px-3 py-2 flex justify-between items-center text-black text-2xl">
-        <div className="font-bold pb-1">Moviez</div>
+        <div className="font-bold">Moviez</div>
         <i
           className="fas fa-bars pr-2"
           onClick={() => setClicked(!clicked)}
