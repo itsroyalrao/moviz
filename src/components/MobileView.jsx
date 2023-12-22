73c95adbbd5e51fe14a7fe45aef3465bcb6bbc7a
Mohit Yadav <my701319@gmail.com>
2023-12-22 14:48:00 +0530

    23th commit

--- src/components/MobileView.jsx
+++ src/components/MobileView.jsx
@@ -95,17 +95,25 @@ function MobileView() {
                 </Link>
               </div>
             </div>
+            <div className="w-full flex items-center">
+              <input
+                type="text"
+                className="w-full px-3 py-[6px] text-black rounded-s focus:outline-none"
+                placeholder="Search..."
+              />
+              <i className="fas fa-search text-2xl text-black bg-yellow-400 px-2 py-[2px] rounded-e cursor-pointer"></i>
+            </div>
           </div>
         </>
       )}
-      <div className="w-full flex items-center p-1">
+      {/* <div className="w-full flex items-center p-1">
         <input
           type="text"
           className="w-full px-3 py-[6px] text-black rounded-s focus:outline-none"
           placeholder="Search..."
         />
         <i className="fas fa-search text-2xl text-black bg-yellow-400 px-2 py-[2px] rounded-e cursor-pointer"></i>
-      </div>
+      </div> */}
     </div>
   );
 }
